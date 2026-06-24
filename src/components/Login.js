31ecf55e8a0cfb4638a559cd80bcc423cc073128
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-06-24 11:22:19 +0530

    Signin/up form Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/validate";
 
 const Login = () => {
   const [isLoginForm, setIsLoginForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidateData(
+      email.current.value,
+      password.current.value,
+    );
+    setErrorMessage(message);
+  };
 
   const toggleSignInForm = () => {
     setIsLoginForm(!isLoginForm);
@@ -16,21 +29,39 @@ const Login = () => {
           alt="background_img"
         />
       </div>
-      <form className="w-3/12 absolute p-9 bg-black my-36 mx-auto right-0 left-0 text-white opacity-95">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-9 bg-black my-36 mx-auto right-0 left-0 text-white opacity-95"
+      >
         <h1 className="font-bold text-3xl py-2">
           {isLoginForm ? "Sign In" : "Sign Up"}
         </h1>
+        {!isLoginForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full rounded-sm  bg-gray-900"
+          ></input>
+        )}
         <input
+          ref={email}
           type="email"
           placeholder="Email Address"
           className="p-2 my-2 w-full rounded-sm bg-gray-900"
         ></input>
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-2 w-full rounded-sm  bg-gray-900"
         ></input>
-        <button className="p-2 my-4 w-full bg-red-700 rounded-sm">
+
+        <p className="text-bold text-lg text-red-700 p-1">{errorMessage}</p>
+
+        <button
+          onClick={handleButtonClick}
+          className="p-2 my-4 w-full bg-red-700 rounded-sm"
+        >
           {isLoginForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
---
+++ src/utils/validate.js
@@ -0,0 +1,10 @@
+export const checkValidateData = (email, password) => {
+  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  if (!isEmailValid) return "Enter a valid email id";
+  if (!isPasswordValid) return "Invalid Password";
+
+  return null;
+};
